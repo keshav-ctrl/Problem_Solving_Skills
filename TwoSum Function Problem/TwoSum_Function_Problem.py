7e4ae19cf20e5af9bd2777545c36fa9a3d63d5c2
kesha <keshavpun27@gmail.com>
2025-04-01 17:07:10 +0545

    Two Sum Functiuon Problem

---
+++ TwoSum Function Problem/TwoSum_Function_Problem.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}
+        for index, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], index]
+            num_map[num] = index
+        return []
+
+S_Object = Solution()
+
+print(S_Object.twoSum([2, 7, 11, 15], 9))
+print(S_Object.twoSum([2, 7, 11, 15], 12))
+print(S_Object.twoSum([2, 7, 11, 15], 22))
